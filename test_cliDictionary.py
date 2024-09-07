30c14379d81ed95ff766a02becc9ba8210422420
Kaki <kaki_github@icloud.com>
2024-09-07 17:39:56 +0300

    test: add unittests
    Adding 7 test cases:
    
    0101 add a word
    0201 list the words
    0301 add an existing words
    0302 don't add an existing words
    0401 remove a word
    0501 store the DB to a file
    0501 load the DB from a file

---
+++ test_cliDictionary.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import unittest
+import cliDictionary
+from unittest.mock import patch
+from io import StringIO
+import sys
+import json
+from pathlib import Path
+import atexit
+import os
+
+
+TEST_DB_PATH = "test.db"
+
+db = cliDictionary.InMemoryDatabase(TEST_DB_PATH)
+
+
+class TestCliDictionary(unittest.TestCase):
+
+    def test_0101_add_word(self):
+        stdin = sys.stdin
+        sys.stdin = open('simulatedInput_add_word.txt', 'r')
+        # cliDictionary.input = lambda _: 'testword'
+        cliDictionary.AddWord().execute()
+        self.assertEqual(db.data["testword"], 'testmeaning')
+        sys.stdin.close()
+        sys.stdin = stdin
+
+    def test_0201_list_words(self):
+        with patch('sys.stdout', new_callable=StringIO) as fake_out:
+            cliDictionary.ListWords().execute()
+            self.assertEqual(fake_out.getvalue().strip(), "testword : testmeaning")
+            fake_out.truncate(0)
+            fake_out.seek(0)
+
+    def test_0301_add_existing(self):
+        stdin = sys.stdin
+        sys.stdin = open('simulatedInput_add_existing_word.txt', 'r')
+        cliDictionary.AddWord().execute()
+        self.assertEqual(db.data["testword"], 'testmeaning1')
+        sys.stdin.close()
+        sys.stdin = stdin
+
+    def test_0302_dont_add_existing(self):
+        stdin = sys.stdin
+        sys.stdin = open('simulatedInput_dont_add_existing_word.txt', 'r')
+        cliDictionary.AddWord().execute()
+        self.assertEqual(db.data["testword"], 'testmeaning1')
+        sys.stdin.close()
+        sys.stdin = stdin
+
+    def test_0401_remove_word(self):
+        stdin = sys.stdin
+        sys.stdin = open('simulatedInput_remove_word.txt', 'r')
+        # cliDictionary.input = lambda _: 'testword'
+        cliDictionary.RemoveWord().execute()
+        removed = "testword" not in db.data
+        self.assertEqual(removed, True)
+        sys.stdin.close()
+        sys.stdin = stdin
+
+    def test_0501_store_db(self):
+        self.test_0101_add_word()
+        db.store_db()
+
+        def cleanup_db_file():
+            os.remove(TEST_DB_PATH)
+        atexit.register(cleanup_db_file)
+        self.assertEqual(Path(TEST_DB_PATH).exists(), True)
+
+    def test_0601_load_db(self):
+        data = None
+        if Path(TEST_DB_PATH).exists():
+            with open(TEST_DB_PATH, "r") as f:
+                data = json.loads(f.read())
+        self.assertEqual(data, {"testword": "testmeaning"})
+
+
+if __name__ == '__main__':
+    unittest.main()
